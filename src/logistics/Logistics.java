a20b7fb54a272c5159728ee3571cd675b315e9a6
Yerassyl Irangait <muratulyrasyl@gmail.com>
2026-09-20 14:11:58 +0500

    feat: implement Logistics abstract creator and concrete creators

---
+++ src/logistics/Logistics.java
@@ -0,0 +1,10 @@
+package logistics;
+import transport.Transport;
+
+public abstract class Logistics {
+    public void planDelivery(String cargo, String destination){
+        Transport transport = createTransport();
+        transport.deliver(cargo, destination);
+    }
+    public abstract Transport createTransport();
+}
